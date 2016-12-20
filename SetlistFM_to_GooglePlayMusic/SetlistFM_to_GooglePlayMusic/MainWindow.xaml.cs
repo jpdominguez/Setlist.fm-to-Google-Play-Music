75ba601f5e9e4021b7aeb4d5912d389c730930c5
Juan P. Dominguez-Morales <jpdominguez@atc.us.es>
2016-12-20 01:27:28 +0100

    New functionalities.
    - Search Artist by Name
    - Search Setlists by Artist
    - Search Songs from Setlist

--- SetlistFM_to_GooglePlayMusic/SetlistFM_to_GooglePlayMusic/MainWindow.xaml.cs
+++ SetlistFM_to_GooglePlayMusic/SetlistFM_to_GooglePlayMusic/MainWindow.xaml.cs
@@ -53,6 +53,7 @@ namespace SetlistFM_to_GooglePlayMusic
         {
             tblock_Artists.Text = "";
 
+            /*
             Uri uri = new Uri("http://api.setlist.fm/rest/0.1/search/artists?artistName=" + tbox_ArtistName.Text.Replace(' ', '-'));
 
             Console.WriteLine(uri.Query);
@@ -64,20 +65,91 @@ namespace SetlistFM_to_GooglePlayMusic
             Stream stream = resp.GetResponseStream();
             TextReader r = new StreamReader(stream);
             Artists im = (Artists)s.Deserialize(r);
+            */
 
+            //foreach (Artist band in im.List)
 
-            foreach (Artist band in im.List)
+            List<Artist> bands = searchArtistByName(tbox_ArtistName.Text.Replace(' ', '-'));
+            foreach (Artist band in bands)
             {
                 tblock_Artists.Text += band.Name;
-                if (im.List.Count > 1)
+                //if (im.List.Count > 1)
+                if(bands.Count > 1)
                 {
-                    tblock_Artists.Text += " \\\\\\ " + band.Disambiguation + "\n";
+                    if (!String.IsNullOrEmpty(band.Disambiguation))
+                    {
+                        tblock_Artists.Text += " (" + band.Disambiguation + ")\n";
+                    }
+                    else
+                    {
+                        tblock_Artists.Text += "\n";
+                    }
                 }
             }
+
+            List<Setlist> setlists = searchSetlistsByArtistName(tbox_ArtistName.Text, 3);
+            List<Song> songs = songsFromSetlist(setlists[0]);
+        }
+
+
+        private List<Artist> searchArtistByName(string artistName)
+        {
+            List<Artist> artists = new List<Artist>();
+
+            Uri uri = new Uri("http://api.setlist.fm/rest/0.1/search/artists?artistName=" + artistName);
+
+            //Console.WriteLine(uri.Query);
+            XmlSerializer s = new XmlSerializer(typeof(Artists));
+
+            //Create the request object
+            WebRequest req = WebRequest.Create(uri);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            TextReader r = new StreamReader(stream);
+            Artists im = (Artists)s.Deserialize(r);
+
+            artists.AddRange(im.List);
+
+            return artists;
+        }
+
+
+        private List<Setlist> searchSetlistsByArtistName(string artistName, int pageNumber)  //each page == 20 results
+        {
+            List<Setlist> setlists = new List<Setlist>();
+
+            Uri uri = new Uri("http://api.setlist.fm/rest/0.1/search/setlists?p="+pageNumber.ToString()+"&artistName=" + artistName);
+
+            //Console.WriteLine(uri.Query);
+            XmlSerializer s = new XmlSerializer(typeof(Setlists));
+
+            //Create the request object
+            WebRequest req = WebRequest.Create(uri);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            TextReader r = new StreamReader(stream);
+            Setlists sets = (Setlists)s.Deserialize(r);
+
+            setlists.AddRange(sets.List.FindAll(x => x.Sets.Count > 0));  //Before adding the setlists to the list, check if the setlist has at least one set.
+
+            return setlists;
+        }
+
+
+        private List<Song> songsFromSetlist(Setlist sl)
+        {
+            List<Song> songs = new List<Song>();
+
+            foreach(Set s in sl.Sets)
+            {
+                songs.AddRange(s.Songs);
+            }
+            return songs;
         }
 
         private void tbox_ArtistName_TextChanged(object sender, TextChangedEventArgs e)
         {
+            /*
             try
             {
                 tblock_Artists.Text = "";
@@ -109,7 +181,8 @@ namespace SetlistFM_to_GooglePlayMusic
 
                 //throw;
             }
-            
+            */
+
         }
     }
 }

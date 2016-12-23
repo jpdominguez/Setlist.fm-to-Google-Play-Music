677188c58c69011787b30b7b8b606cd0a3560fb2
Juan P. Dominguez-Morales <jpdominguez@atc.us.es>
2016-12-23 02:44:35 +0100

    Working on ListBoxItem with buttons

---
+++ SetlistFM_to_GooglePlayMusic/SetlistFM_to_GooglePlayMusic/Controls/ArtistListItem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SetlistFM_to_GooglePlayMusic.Controls
+{
+    /// <summary>
+    /// Interaction logic for ArtistListItem.xaml
+    /// </summary>
+    public partial class ArtistListItem : UserControl
+    {
+        public ArtistListItem()
+        {
+            InitializeComponent();
+        }
+    }
+}

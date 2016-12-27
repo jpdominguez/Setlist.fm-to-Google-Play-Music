ae52eaa659bebad0adc7f983a3fab18df58de480
Juan P. Dominguez-Morales <jpdominguez@atc.us.es>
2016-12-27 02:23:50 +0100

    Update

--- SetlistFM_to_GooglePlayMusic/SetlistFM_to_GooglePlayMusic/Controls/SetlistViewer.xaml.cs
+++ SetlistFM_to_GooglePlayMusic/SetlistFM_to_GooglePlayMusic/Controls/SetlistViewer.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fm.Setlist.Api.Model;
 
 namespace SetlistFM_to_GooglePlayMusic.Controls
 {
@@ -23,6 +26,14 @@ namespace SetlistFM_to_GooglePlayMusic.Controls
         public SetlistViewer()
         {
             InitializeComponent();
+
+            var items = new List<int>();
+            items.Add(4);
+            items.Add(2);
+            items.Add(3);
+
+
+            dataGrid_log.ItemsSource = items;
         }
     }
 }

9ec0a8fedb83d8af1cb36c91c7a6d18e9c9bf3e7
Juan Pedro Dominguez-Morales <jpdominguez@atc.us.es>
2017-03-10 20:15:23 +0100

    Updated

--- SetlistFM_to_GooglePlayMusic/SetlistFM_to_GooglePlayMusic/Controls/SetlistViewer.xaml.cs
+++ SetlistFM_to_GooglePlayMusic/SetlistFM_to_GooglePlayMusic/Controls/SetlistViewer.xaml.cs
@@ -27,13 +27,23 @@ namespace SetlistFM_to_GooglePlayMusic.Controls
         {
             InitializeComponent();
 
-            var items = new List<int>();
-            items.Add(4);
-            items.Add(2);
-            items.Add(3);
+            DataGridTextColumn c1 = new DataGridTextColumn();
+            c1.Header = "Num";
+            c1.Binding = new Binding("Num");
+            c1.Width = 110;
+            dataGrid_log.Columns.Add(c1);
+            DataGridTextColumn c2 = new DataGridTextColumn();
+            c2.Header = "Start";
+            c2.Width = 110;
+            c2.Binding = new Binding("Start");
+            dataGrid_log.Columns.Add(c2);
+            DataGridTextColumn c3 = new DataGridTextColumn();
+            c3.Header = "Finich";
+            c3.Width = 110;
+            c3.Binding = new Binding("Finich");
+            dataGrid_log.Columns.Add(c3);
 
-
-            dataGrid_log.ItemsSource = items;
+            dataGrid_log.Items.Add(new Setlist() { EventDate = "Prueba", Tour = "2012, 8, 15", Venue = "2012, 9, 15" });
         }
     }
 }
